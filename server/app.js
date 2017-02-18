ecaabec6d95e18dad793b96a5e7eb72d9ead2d96
Sahaj Garg <sahajgarg@gmail.com>
2017-02-18 11:44:29 -0800

    Delete mongo

--- server/app.js
+++ server/app.js
@@ -5,18 +5,8 @@
 'use strict';
 
 import express from 'express';
-import mongoose from 'mongoose';
-mongoose.Promise = require('bluebird');
 import config from './config/environment';
 import http from 'http';
-import seedDatabaseIfNeeded from './config/seed';
-
-// Connect to MongoDB
-mongoose.connect(config.mongo.uri, config.mongo.options);
-mongoose.connection.on('error', function(err) {
-  console.error(`MongoDB connection error: ${err}`);
-  process.exit(-1); // eslint-disable-line no-process-exit
-});
 
 // Setup server
 var app = express();
@@ -31,7 +21,6 @@ function startServer() {
   });
 }
 
-seedDatabaseIfNeeded();
 setImmediate(startServer);
 
 // Expose app
