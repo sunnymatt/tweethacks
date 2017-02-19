a7007424caf97b9081bafac77bb5c37a2dfa60e9
Matthew Sun <matthewkol186@gmail.com>
2017-02-18 20:37:14 -0800

    frontend stuff

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -6,6 +6,11 @@ export class MainController {
 
   awesomeThings = [];
   newThing = '';
+  searchExec = false;
+  username = '';
+  friendname = '';
+  usersToCheck = {};
+  friendsTweets = {};
 
   /*@ngInject*/
   constructor($http) {
@@ -32,12 +37,57 @@ export class MainController {
   deleteThing(thing) {
     //this.$http.delete(`/api/things/${thing._id}`);
   }
+
+  addFollowersFromHandle(username) {
+    this.username = "";
+    var self = this;
+    this.$http.get('/followers/' + username)
+      .then(response => {
+        for (var follower in response.data) {
+          this.usersToCheck[Object.keys(this.usersToCheck).length] = response.data[follower]; // add one by one
+        }
+      });
+  }
+
+  addUserToCheck(handle) {
+    this.usersToCheck[Object.keys(this.usersToCheck).length] = handle; // add one by one
+    this.friendname = "";
+  }
+
+  removeUserToCheck(index) {
+    delete this.usersToCheck[index];
+  }
+
+  runSearch() {
+    var self = this;
+    this.$http({
+      method: 'GET',
+      url: '/twitter',
+      headers: {
+        names: JSON.stringify(this.usersToCheck)
+      }
+    }).then(response => {
+      self.friendsTweets = response.data;
+      console.log(self.friendsTweets);
+      self.searchExec = true;
+      self.usersToCheck = {};
+    });
+  }
+
+  canCheckUsers() {
+    return Object.keys(this.usersToCheck).length > 0;
+  }
+
+  goBack() {
+    this.searchExec = false;
+  }
 }
 
 export default angular.module('tweethacksApp.main', [uiRouter])
   .config(routing)
   .component('main', {
     template: require('./main.html'),
-    controller: MainController
+    controller: MainController,
+    controllerAs: 'mc'
   })
   .name;
